1ef63754d8cbd114eb623a0c4da4abff233b6218
GessicaP <projetos.gessica@gmail.com>
2023-04-13 09:11:51 -0300

    Salvando dados e refatorando código.

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,9 +1,9 @@
+import { Course } from './../model/course';
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable, delay, first, take, tap } from 'rxjs';
-import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +18,12 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      delay(500),
       tap(courses => console.log(courses))
     );
   }
+
+  save(record: Course){
+    return this.httpClient.post<Course>(this.API, record).pipe(first())
+  }
+
 }
